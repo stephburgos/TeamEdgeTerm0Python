dac5cb6c1799753aad83a2e9a642211970b42e59
coach-stephanie <43859479+coach-stephanie@users.noreply.github.com>
2022-07-10 15:21:13 -0400

    Test variables

--- variables.py
+++ variables.py
@@ -18,9 +18,11 @@ print(message)
 
 print("\n------------------- Challenge 1 -------------------\n")
 
-#print(f"Hi! My name is {name}")
-#print(f"I'm in the {grade}th grade.")
-#print(f"A fun fact about me is that {funFact}")
+name = "Stephanie"
+
+print(f"Hi! My name is {name}")
+print(f"I'm in the {grade}th grade.")
+print(f"A fun fact about me is that {funFact}")
 
 
 # **** Challenge 1: Problem 1 ****
