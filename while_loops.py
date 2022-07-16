a0730e53735d3ebc626cd19afe204bb6049433cd
coach-stephanie <43859479+coach-stephanie@users.noreply.github.com>
2022-07-15 20:27:06 -0400

    While loops challenge

--- while_loops.py
+++ while_loops.py
@@ -16,13 +16,16 @@
  # 
  # ***************************************************************/
 
+import random
+
+
 print("------------------- CHALLENGE 1 : IN YOUR PRIME -------------------")
 
 #Here is a humble while loop in action. We need a variable to hold the counter value.
 num = 0
 while num <= 10:
-    print("example counter--> " + str(num))
-    num += 1
+  print("example counter--> " + str(num))
+  num += 1
 
 
 #------------ Helper function, do not mess with this part below ---------------??
@@ -41,7 +44,12 @@ def test_prime(n):
     return True
 
 #-->TODO: Declare a while loop that prints all the prime numbers between 0 and 100, use test_prime() helper function
+num = 0 
+while num <= 100: 
+  if test_prime(num):
+    print(num)
 
+  num += 1
 
 
 print("------------------- CHALLENGE 2 : FOUND   -------------------")
@@ -52,6 +60,13 @@ items = ["pencil" , "eraser" , "mirror" , "comb" , "spoon" , "key" , "earrings" 
 #-->TODO: Use a while loop to search the contents of a list for the key! If it exists, print "found the key!"
 
 
+index = 0 
+while index < len(items):
+  if items[index] == "key": 
+    print("Found the key!")
+  
+  index += 1
+
 
 print("------------------- CHALLENGE 3 : BUGGIN   -------------------")
 
@@ -61,9 +76,11 @@ print("------------------- CHALLENGE 3 : BUGGIN   -------------------")
 #-->TODO: Make me count  2, 4, 6,..., 50
 
 def even_numbers_to_fifty():
-    num = 50
-    while num < 50:
+    num = 0
+    while num <= 50:
+      if num % 2 == 0: 
         print("number: " + str(num))
+      num += 1
 
 even_numbers_to_fifty()
 
@@ -85,13 +102,18 @@ even_numbers_to_fifty()
 
 def pattern():
 
-    index = 0 
-    my_list =[]
-    
-    while index <= 5:
-        my_list.append(index)
-        print(my_list)
-        index += 1
+  index = 0 
+  my_list =[]
+  
+  while index <= 5:
+    my_list.append(index)
+    print(my_list)
+    index += 1
+
+  while index > 1:
+    my_list.pop()
+    print(my_list)
+    index -= 1
 
 pattern()
 
@@ -100,11 +122,21 @@ print("------------------- CHALLENGE 4 : MATH QUIZ   -------------------")
 
 
 
-#-->TODO: Make a Math Quiz that asks two random numbers (between 0 and 100 to make it easy).
+#-->TODO: Make a Math Quiz that adds two random numbers (between 0 and 100 to make it easy).
 #         The user enters the answer. If wrong, keep prompting. If correct, say congrats!!
 #         Use this handy boolean to get you started! You will need input()!
 
 is_correct = False
+num1 = random.randint(0, 101)
+num2 = random.randint(0, 101)
+correct_answer = num1 + num2 
+user_answer = int(input(f"What is {num1} + {num2}? "))
+
+while user_answer != correct_answer:
+  print("Try again!")
+  user_answer = int(input(f"What is {num1} + {num2}? "))
+
+print(f"Correct! {num1} + {num2} = {correct_answer}")
 
 
 print("------------------- CHALLENGE 5 : WHAT AM I?   -------------------")
@@ -113,20 +145,40 @@ print("------------------- CHALLENGE 5 : WHAT AM I?   -------------------")
 #         You are given two starter functions and a loop to get started! 
 #         Notice how one function calls the other and uses the returned value as the input. This is called Recursion! 
 
-keep_asking = False
+#Solution 
+keep_asking = True
 
 def prompt_user():
-    pass
+  response = input("Enter a word: ")
+  return response
 
 def response(response):
-    pass
+  print(f"I know you are a {response}, but what am I?")
 
 while keep_asking:
-    #response(prompt_user())
-    pass
+  response(prompt_user())
 
 #-->TODO: Challenge! write a secret word to break out of the loop!
 
+#Solution + secret word
+keep_asking = True
+secret_word = "amazing"
+
+def prompt_user():
+  response = input("Enter a word: ")
+  return response
+
+def response(response):
+  print(f"I know you are a {response}, but what am I?")
+
+while keep_asking:
+  user_response = prompt_user()
+  if user_response == secret_word:
+    keep_asking = False
+  else: 
+    response(user_response)
+
+print("You guessed the secret word!")
 
 
 
